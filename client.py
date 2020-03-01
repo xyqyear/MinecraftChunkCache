1576cfc4f451b3c646a649d49940535b0b841134
xyqyear <215908558@qq.com>
2020-03-01 17:26:43 +0800

    clean up

--- client.py
+++ client.py
@@ -13,10 +13,8 @@ dst_port = 2000
 @client_auto_unpack_pack
 def tweak(buff: Buffer1_7) -> bytes:
     packet_id = buff.unpack_varint()
-    if packet_id == 0x22 and False:
+    if packet_id == 0x22:
         packet_chunk_data = PacketChunkData(buff.buff[buff.pos:])
-        print(f'{len(buff.buff)}, {len(b"".join(i for i in packet_chunk_data.sections if i))},'
-              f' {len(packet_chunk_data.biomes) if packet_chunk_data.full else 0}\n')
         return buff.pack_varint(packet_id) + packet_chunk_data.pack_packet_data()
 
     return buff.buff
